cd7b9261fc35d9a00228da257311614e0467cd34
Matias Salibian-Barrera <matias@stat.ubc.ca>
2019-07-23 16:39:47 -0700

    clean up general example

--- FPLM-Bsplines-example.R
+++ FPLM-Bsplines-example.R
@@ -9,13 +9,14 @@ source('FPLM-Bsplines-functions.R')
 library(fda.usc)
 data(tecator)
 ## Prepare training set
+## splines approximation to the second derivative
+absorp2 <- fdata.deriv(tecator$absorp.fdata, nderiv = 2)
 y  <- tecator$y$Fat[1:155]
 u  <- tecator$y$Protein[1:155]
-ab <- tecator$absorp.fdata
-t  <- ab$argvals
-x  <- ab$data[1:155,]
+t  <- absorp2$argvals
+x  <- absorp2$data[1:155,]
 
-## Model: fat = < beta, absorp> + eta(u)
+## Model: fat = < beta, absorp2> + eta(u)
 criterion <- 'bic1'
 ## range for possible dimension for splines for the regression component
 range_beta <- 4:13
@@ -43,7 +44,8 @@ plot(u[u_order], d$fit$eta_est[u_order], type = "l", xlab = "Protein", pch = 1,
      lwd = 6, col = "black", ylab = expression(hat(eta)), lty=1, ylim=c(mi, ma))
 lines(u[u_order], a$fit$eta_est[u_order], lwd = 6, col = "tomato3")
 lines(u[u_order], b$fit$eta_est[u_order], lwd = 6, col = "skyblue3")
-
+legend('bottomright', lty=1, lwd=3, col=c('black', 'tomato3', 'skyblue3'),
+       legend=c('M', 'MM', 'LS'))
 ## Plot robust hat(beta)
 ma <- max(rrb <- c(a$fit$slope_fun, b$fit$slope_fun, d$fit$slope_fun) )
 mi <- min(rrb)
@@ -51,4 +53,6 @@ plot(t, a$fit$slope_fun, col = "black", lwd = 6,
      type = "l", xlab = "Wavelength", ylab = expression(hat(beta)), lty=1, ylim=c(mi, ma))
 lines(t, d$fit$slope_fun, col = "tomato3", lwd = 6, lty=1)
 lines(t, b$fit$slope_fun, col = "skyblue3", lwd = 6, lty=1)
+legend('bottom', lty=1, lwd=3, col=c('black', 'tomato3', 'skyblue3'),
+       legend=c('M', 'MM', 'LS'))
 
