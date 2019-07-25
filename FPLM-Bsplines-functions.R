5052e7e4c9766e70c55527281f690d9fbe9b69b8
Matias Salibian-Barrera <matias@stat.ubc.ca>
2019-07-25 15:29:29 -0700

    bug

--- FPLM-Bsplines-functions.R
+++ FPLM-Bsplines-functions.R
@@ -20,24 +20,24 @@ minimize <- function (yy, xx_coef, uu, spl_kn, freq, fLoss, norder, pars,
                                                 k.max       = 2e3,       # 200
                                                 maxit.scale = 2e3,       # 200
                                                 max.it      = 2e3)) {
-  
+
   ## Initialize
   cv  <- tr <- aicM <- NA
-  
+
   ## B-spline basis
   kns    <- seq(min(uu), max(uu), length = spl_kn - norder + 2)
   base   <- create.bspline.basis(rangeval = range(uu),
                                  norder = norder,
                                  breaks = kns)
   spl_uu <- getbasismatrix(uu, base)
-  
+
   ## Design matrix
-  
+
   X <- cbind(xx_coef, spl_uu)
-  
+
   if( !(fLoss %in% c('ls','huang', 'lmrob') ) )
     stop("Invalid fLoss. Should be one of \'ls\' or \'huang\' or \'lmrob\' ")
-  
+
   ## Minimization menu
   switch(fLoss,
          ls = {
@@ -70,14 +70,14 @@ minimize <- function (yy, xx_coef, uu, spl_kn, freq, fLoss, norder, pars,
          },
          stop('Invalid fLoss.')
   )
-  
+
   ## Estimated parameters
   # intercept <- cf[1]
   # slope_par <- cf[2:(freq + 1)]
   # spl_par <- cf[-(1:(freq + 1))]
   spl_par   <- cf[-(1:freq)]
   slope_par <- cf[  1:freq ]
-  
+
   return(list(spl = spl_par, slope = slope_par, #intercept = intercept,
               value = vv, scale = ss, conv = cv, traza = tr, matias = aicM))
 }
@@ -85,11 +85,11 @@ minimize <- function (yy, xx_coef, uu, spl_kn, freq, fLoss, norder, pars,
 
 FPLMBsplines_fit <- function (y, x, u, t, freq, spl,
                               norder, fLoss) {
-  
+
   ## Integration step
   dt <- min(diff(t))  # width of grid
   xcenter <- x
-  
+
   ## Covariance decomposition
   grilla_spl <- t #seq(0, 1, length = length(t))
   nodos_spl <- seq(min(grilla_spl), max(grilla_spl),
@@ -99,16 +99,16 @@ FPLMBsplines_fit <- function (y, x, u, t, freq, spl,
                                    breaks = nodos_spl)
   beta_spl <- getbasismatrix(grilla_spl, base_spl)
   cov_dec <- beta_spl[, 1:freq]
-  
+
   ## Estimated Fourier coefficients (by row)
   xx_coef <- xcenter %*% cov_dec * dt
-  
+
   ## Parameter estimation
   est <- minimize(y, xx_coef, u, spl, freq, fLoss,
                   norder, pars)
-  
+
   est$slope_fun <- cov_dec %*% est$slope
-  
+
   return (est)
 }
 
@@ -140,19 +140,21 @@ goodness <- function(nn, scl, val, spl,freq, criterion){
 
 
 FPLMBsplines <- function (y, x, u, t, range_freq = (floor(max(n^(1/5), norder)):floor(2*(norder+n^(1/5)))),
-                          range_spl= (floor(max(n^(1/5), norder)):floor(2*(norder+n^(1/5)))), 
+                          range_spl= (floor(max(n^(1/5), norder)):floor(2*(norder+n^(1/5)))),
                           norder=4, fLoss='lmrob', criterion = 'bic1',
                           trace=FALSE) {
-  
+
   opt <- spl_opt <- freq_opt <- fit_opt <- Inf
-  
+
+  n <- length(y)
+
   for(spl in range_spl) {
     for(freq in range_freq) {
       fit <- FPLMBsplines_fit(y, x, u, t, freq, spl, norder, fLoss)
       val <- fit$value
       scl <- fit$scale
-      nn  <- length(y)
-      crt <- goodness(nn, scl, val, spl, freq, criterion)
+      # nn  <- length(y)
+      crt <- goodness(n, scl, val, spl, freq, criterion)
       if (crt < opt) {
         opt      <- crt
         spl_opt  <- spl
@@ -162,7 +164,7 @@ FPLMBsplines <- function (y, x, u, t, range_freq = (floor(max(n^(1/5), norder)):
       if(trace) print(c('spl' = spl, 'freq' = freq, 'crit' = crt))
     }
   }
-  
+
   kns    <- seq(min(u), max(u), length = spl_opt - norder + 2)
   base   <- create.bspline.basis(rangeval = range(u),
                                  norder = norder,
